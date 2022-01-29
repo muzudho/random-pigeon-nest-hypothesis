8160595ebfb5767e72ef2ffb2f0b6347902f5873
むずでょ <muzudho1@gmail.com>
2022-01-30 00:22:34 +0900

    マジックナンバーは１８（＾～＾）

--- pigeon.py
+++ pigeon.py
@@ -1,7 +1,8 @@
 import math
 
 # 定数
-a = 2 * math.pi * math.e
+a = 2 * math.pi * math.e # 理論値。ボールが１個見つからないぐらいのギリギリの精度
+# a = (2 * math.pi * math.e) + 1 # 理論値要らなければ 1 でも足しとけば 遊びで使うのに十分な いい感じ（＾～＾）
 
 def trial_count(N):
     return math.ceil(N * a)
